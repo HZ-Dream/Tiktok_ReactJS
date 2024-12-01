959a4343655a469d66b2e91df6d182bafde81fff
Ngoc Hoai <ngochoai5813@gmail.com>
2024-12-01 20:00:58 +0700

    Add Contentjs

---
+++ src/Content.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react"
+
+function Content() {
+
+    useEffect (() => {
+        
+    })
+    return (
+        <div>
+            <h1>Hello anh em Man City và SGP</h1>
+        </div>
+    )
+}
+
+export default Content
