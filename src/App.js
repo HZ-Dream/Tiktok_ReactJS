5ba91499f6d23a5cd1de418e9ce90118e2c9622b
Ngoc Hoai <ngochoai5813@gmail.com>
2024-12-24 09:56:20 +0700

    Logic Header 2

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ import { DefaultLayout } from '~/components/Layout';
 function App() {
     return (
         <Router>
-            <div className="app">
+            <div className="App">
                 <Routes>
                     {publicRoutes.map((route, index) => {
                         const Page = route.component;
-
                         let Layout = DefaultLayout;
 
                         if (route.layout) {
@@ -18,6 +17,7 @@ function App() {
                         } else if (route.layout === null) {
                             Layout = Fragment;
                         }
+
                         return (
                             <Route
                                 key={index}
--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,27 +1,24 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src="https://scontent.fsgn16-1.fna.fbcdn.net/v/t39.30808-6/448020594_986596983474571_4865932017320470926_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=Vv3p2GVFb_YQ7kNvgFruFxW&_nc_zt=23&_nc_ht=scontent.fsgn16-1.fna&_nc_gid=AZvG_rYyS1DsNToThl-ECGi&oh=00_AYCX_XLMiHEUID8uhUaR8_Z4jD_LjDLF5bCmdMY7A0uLew&oe=67686079"
-                alt="SGP"
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Saigon Phantom</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>saigonphantom.aov</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
