76e873100796a60c07b2c17b197f080e3f86a1fd
Ngoc Hoai <ngochoai5813@gmail.com>
2025-01-03 10:06:06 +0700

    Fix Sidebar 1

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -4,6 +4,10 @@ const httpRequest = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
 });
 
+// https://files.fullstack.edu.vn/f8-tiktok/videos/3624-6669c160c13f5.mp4
+// https://tiktok.fullstack.edu.vn/api/users/suggested?per_page=1&page=50
+// https://tiktok.fullstack.edu.vn/api/videos?type=for-you&page=1
+
 export const get = async (path, options = {}) => {
     const response = await httpRequest.get(path, options);
     return response.data;
